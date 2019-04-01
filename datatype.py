81af008981c592c675a8bf44a75ce1695487d81f
PascuFCalvo <Pascual.fernandez.calvo>
2019-04-01 09:44:54 +0200

    Añadiendo Hello y piticli

---
+++ datatype.py
@@ -0,0 +1,55 @@
+
+#Strings, cadenas de caracteres
+print("Hello World")
+print('Hello World')
+print('''Hello World''')
+print("""Hello World""")
+
+#funcion type para saber que clase de dato es
+#englobada dentro de la funcion print para poder sacarla por pantalla
+print(type("Hello World"))
+
+#Unir Strings [concatenacion]
+#operacion print para mostrarlo por pantalla
+print("Bye"+" World")
+
+
+#Numbers
+
+#enteros
+print(30)
+
+#float
+print(30.5)
+
+#boolean, estados logicos
+True
+False
+
+#List (Agrupar datos que peuden cambiar)
+[10, 20, 30, 55]
+["Hello","Hola", "Adios"]
+[True,"Hello",35]
+[]
+
+#Tuples (Agrupar datos que no pueden cambiar)
+(10,20,30,55)
+()
+
+#Dictionaries (Agrupar datos de una misma entidad)
+{
+    "nombre":"Ryan",
+    "apellido":"Ray",
+    "apodo":"Rusboi"
+}
+
+
+#probando a imprimir que typo de clase
+print(type({
+    "nombre":"Ryan",
+    "apellido":"Ray",
+    "apodo":"Rusboi"
+}))
+
+#Cuando aun no esta definido como un tipo de dato
+None
